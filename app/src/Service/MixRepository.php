6ef7aa585557abb5198b91c044791c4980c455f4
plankconstanta <pushkarevaa@yandex.ru>
2023-02-16 20:30:55 +0300

    inject via attribute, autowiring service with help of name

--- app/src/Service/MixRepository.php
+++ app/src/Service/MixRepository.php
@@ -3,14 +3,16 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MixRepository
 {
     public function __construct(
-        private HttpClientInterface $httpClient,
+        private HttpClientInterface $githubContentClient,
         private CacheInterface $cache,
+        #[Autowire('%kernel.debug%')]
         private bool $isDebug
     ) {}
 
@@ -18,8 +20,8 @@ class MixRepository
     {
         $mixes = $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
-            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
-
+            //$response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
         });
         return $mixes;

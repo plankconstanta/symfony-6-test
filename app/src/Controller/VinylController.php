359ecfde365d25295f84eec2ae99b191ec1b0d6a
plankconstanta <pushkarevaa@yandex.ru>
2023-02-17 12:27:45 +0300

    use autowire for injecting non-autowwirable params

--- app/src/Controller/VinylController.php
+++ app/src/Controller/VinylController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Twig\Environment;
 use function Symfony\Component\String\u;
 
--- app/src/Service/MixRepository.php
+++ app/src/Service/MixRepository.php
@@ -6,6 +6,9 @@ use Psr\Cache\CacheItemInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class MixRepository
 {
@@ -13,11 +16,17 @@ class MixRepository
         private HttpClientInterface $githubContentClient,
         private CacheInterface $cache,
         #[Autowire('%kernel.debug%')]
-        private bool $isDebug
+        private bool $isDebug,
+        #[Autowire(service: 'twig.command.debug')]
+        private DebugCommand $twigDebugCommand,
     ) {}
 
     public function findAll(): array
     {
+        $output = new BufferedOutput();
+        $this->twigDebugCommand->run(new ArrayInput([]), $output);
+        dd($output);
+
         $mixes = $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
             //$response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');

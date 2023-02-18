b67886a7f7e2a064250d7d802b786b42cae13242
plankconstanta <pushkarevaa@yandex.ru>
2023-02-19 00:59:47 +0300

    add service to command

--- app/src/Command/TalkToMeCommand.php
+++ app/src/Command/TalkToMeCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\MixRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -16,6 +17,13 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class TalkToMeCommand extends Command
 {
+    public function __construct(
+        private MixRepository $mixRepository
+    )
+    {
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this
@@ -33,6 +41,11 @@ class TalkToMeCommand extends Command
             $message = strtoupper($message);
         }
         $io->success($message);
+        if ($io->confirm('Do you want a mix recommendation?')) {
+            $mixes = $this->mixRepository->findAll();
+            $mix = $mixes[array_rand($mixes)];
+            $io->note('I recommend the mix: ' . $mix['title']);
+        }
         return Command::SUCCESS;
     }
 }

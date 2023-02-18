4b099a73396355bd81787df8e5c15739cc8d061f
plankconstanta <pushkarevaa@yandex.ru>
2023-02-19 00:51:43 +0300

    command: simple cases of input/output

--- app/src/Command/TalkToMeCommand.php
+++ app/src/Command/TalkToMeCommand.php
@@ -19,26 +19,20 @@ class TalkToMeCommand extends Command
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+            ->addArgument('name', InputArgument::OPTIONAL, 'Your name')
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'Shall I yell?');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $name = $input->getArgument('name') ?: 'whoever you are';
+        $shouldYell = $input->getOption('yell');
+        $message = sprintf('Hey %s!', $name);
+        if ($shouldYell) {
+            $message = strtoupper($message);
         }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-
+        $io->success($message);
         return Command::SUCCESS;
     }
 }
